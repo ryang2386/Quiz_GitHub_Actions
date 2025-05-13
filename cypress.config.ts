e0cde3239599414040b788699f2623dccab05796
ryang2386 <gayler02@nyu.edu>
2025-05-13 18:39:01 -0400

    first commit

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress';
+import viteConfig from './vite.config';
+
+export default defineConfig({
+  component: {
+    port: 3001,
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+      viteConfig,
+    },
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
